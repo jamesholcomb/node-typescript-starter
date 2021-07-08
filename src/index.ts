e41f39ed0d932b2b2ec56d86dc8b98df9a2f78b6
James Holcomb <james.holcomb@gmail.com>
2021-07-08 14:19:07 -0500

    chore: update package versions prettier no semi, no single-quote format on save

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { isPrimeNumber } from './prime';
+import { isPrimeNumber } from "./prime"
 
-const n = Math.floor(Math.random() * 1000);
+const n = Math.floor(Math.random() * 1000)
+
 if (isPrimeNumber(n)) {
-  console.log(`The number ${n} is a prime number.`);
+  console.log(`The number ${n} is a prime number.`)
 } else {
-  console.log(`The number ${n} is not a prime number.`);
+  console.log(`The number ${n} is not a prime number.`)
 }
--- src/prime.ts
+++ src/prime.ts
@@ -1,6 +1,7 @@
 export const isPrimeNumber = (n: number): boolean => {
   for (let i = 2; i < n; i++) {
-    if (n % i === 0) return false;
+    if (n % i === 0) return false
   }
-  return n > 1;
-};
+
+  return n > 1
+}
